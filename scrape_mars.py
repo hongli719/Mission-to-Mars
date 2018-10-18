50bec935586c816214eb93c9bce573dac1d0960d
hongli719 <hongli719@gmail.com>
2018-10-18 14:38:45 -0700

    finish the rest and working on the hemisphere part

--- scrape_mars.py
+++ scrape_mars.py
@@ -19,43 +19,58 @@ def scrape():
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
 
-    listings["news title"] = soup.find('div', class_='content_title').get_text()
-    listings["news p"] = soup.find('div', class_='article_teaser_body').get_text()
+    listings["news_t"] = soup.find('div', class_='content_title').get_text()
+    listings["news_p"] = soup.find('div', class_='article_teaser_body').get_text()
     
     url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(url)
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
 
-    listings["weather"] = soup.find('div', class_='js-tweet-text-container').text.strip()
+    featured_image_url = soup.find('a', class_='button fancybox')['data-fancybox-href']
+    listings["image_url"] = 'https://www.jpl.nasa.gov' + featured_image_url
+
+    url = 'https://twitter.com/marswxreport?lang=en'
+    browser.visit(url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    tweet = soup.find_all('div', class_='js-tweet-text-container')
+    for result in tweet:
+        x = result.find('p', class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text").text
+        x = x.split(' ')
+        if x[0] == 'Sol':
+            listings["weather"] = result.find('p', class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text").get_text()
+            break
+
 
     url = 'https://space-facts.com/mars/'
     tables = pd.read_html(url)
     df = tables[0]
-    df.set_index(0, inplace=True)
+    df.columns = ['', 'value']
+    df.set_index('', inplace=True)
 
     html_table = df.to_html()
     html_table = html_table.replace('\n', '')
 
     listings["table"] =  html_table
 
-    # url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-    # browser.visit(url)
-    # html = browser.html
-    # soup = BeautifulSoup(html, 'html.parser')
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
 
-    # dict = {'title':'','img url':''}
+    lst = []
 
-    # results = soup.find_all('div', class_='item')
-    # for result in results:
-    #     link_text = result.find('h3').text
+    results = soup.find_all('div', class_='item')
+    for result in results:
+        link_text = result.find('h3').text
     #     print(link_text)
     #     browser.click_link_by_partial_text(link_text)
-    #     t = soup.find('h2', class_='title')
-    #     print(t)
-    #     dict['title'] = t
-    #     dict['img url'] = soup.find('img', class_='wide-large')['src']
+    #     img_url = soup.find('img', class_='wide-large')['src']
+        lst.append({'title':link_text})
     #     browser.back()
 
+    listings["hemisphere"] = lst
 
     return listings
